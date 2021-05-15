5825fa1ed2ba6f60021767ac872eee77babc08f4
Niger Yanes <nyanes@focasoftware.com>
2021-05-14 23:38:35 -0500

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name' => 'Niller Yanes',
+            'email' => 'nigeryanes@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
+    }
+}
